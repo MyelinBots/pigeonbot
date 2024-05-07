290bdef32c8409469ea8ced85d2582d911f99bfd
James Trotter <james.trotter@tempmee.com>
2024-05-07 19:56:16 -0400

    init

---
+++ modules/pigeon/actions.py
@@ -0,0 +1,28 @@
+import random
+
+
+class Action:
+    def __init__(self, action: str, items: list, format: str, actionPoint: int):
+        self.action = action
+        self.items = items
+        self.format = format
+        self.actionPoint = actionPoint
+
+    def actionPoint(self):
+        return self.actionPoint
+
+    def action(self):
+        return self.action
+
+    def items(self):
+        return self.items
+
+    def format(self):
+        return self.format
+
+    def act(self):
+        # choose random item
+        item = random.choice(self.items)
+
+        return self.format % (self.action, item)
+
---
+++ modules/pigeon/player.py
@@ -0,0 +1,21 @@
+startingPoints: int = 10000
+
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.points = startingPoints
+
+    def name(self):
+        return self.name
+
+    def points(self):
+        return self.points
+
+    def changePoints(self, points: int):
+        self.points += points
+
+    def resetPoints(self):
+        self.points = startingPoints
+
+    def __str__(self):
+        return f"{self.name} has {self.points} points."
