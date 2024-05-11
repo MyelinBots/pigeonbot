487f8a9f80f1cff2145a697c236e24168b019f4b
James Trotter <james.trotter@tempmee.com>
2024-05-11 14:53:00 -0400

    refactor

---
+++ modules/pigeon/game.py
@@ -0,0 +1,42 @@
+from .actions import Action
+from .player import Player
+from .timer import RepeatedTimer
+import random
+class Game:
+    def __init__(self, irc):
+        self.irc = irc
+        self.players: [Player] = []
+        self.actions: [Action] = [
+            Action("stole", ["tv", "wallet", "food"], "a pigeon %s your %s", 10),
+            Action("pooped", ["car", "table", "head"], "a pigeon %s on your %s", 10),
+            Action("landed", ["balcony", "head", "car", "house"], "a pigeon has %s on your %s", 10),
+        ]
+
+    def addPlayer(self, name: str) -> None:
+        for player in self.players:
+            if player.name == name:
+                return
+        self.players.append(Player(name))
+
+    def removePlayer(self, name: str) -> None:
+        for player in self.players:
+            if player.name == name:
+                self.players.remove(player)
+                return
+
+    def actOnPlayer(self) -> None:
+        print("Going to trigger pigeon attack")
+        print(len(self.players))
+        if len(self.players) == 0:
+            print("No players to act on")
+            return
+        player = random.choice(self.players)
+        action = random.choice(self.actions)
+        # player.changePoints(action.actionPoint)
+        self.irc.privmsg(self.irc.config.channel,  action.act())
+
+    def start(self):
+        print("dones something")
+        # do interval for every 5 seconds
+        RepeatedTimer(10, self.actOnPlayer)
+
--- modules/pigeon/main.py
+++ modules/pigeon/main.py
@@ -1,12 +1,6 @@
-import threading
-import random
-
-from .actions import Action
-from .player import Player
 from pyircsdk import Module
 from .commands import Commands
-from .timer import RepeatedTimer
-
+from .game import Game
 
 
 class PigeonModule(Module):
@@ -56,42 +50,4 @@ class PigeonModule(Module):
                 self.irc.privmsg(message.messageTo, "The pigeon module is not implemented yet.")
                 return
 
-
-class Game:
-    def __init__(self, irc):
-        self.irc = irc
-        self.players: [Player] = []
-        self.actions: [Action] = [
-            Action("stole", ["tv", "wallet", "food"], "a pigeon %s your %s", 10),
-            Action("pooped", ["car", "table", "head"], "a pigeon %s on your %s", 10),
-            Action("landed", ["balcony", "head", "car", "house"], "a pigeon has %s on your %s", 10),
-        ]
-
-    def addPlayer(self, name: str) -> None:
-        for player in self.players:
-            if player.name == name:
-                return
-        self.players.append(Player(name))
-
-    def removePlayer(self, name: str) -> None:
-        for player in self.players:
-            if player.name == name:
-                self.players.remove(player)
-                return
-
-    def actOnPlayer(self) -> None:
-        print("Going to trigger pigeon attack")
-        print(len(self.players))
-        if len(self.players) == 0:
-            print("No players to act on")
-            return
-        player = random.choice(self.players)
-        action = random.choice(self.actions)
-        # player.changePoints(action.actionPoint)
-        self.irc.privmsg(self.irc.config.channel,  action.act())
-
-    def start(self):
-        print("dones something")
-        # do interval for every 5 seconds
-        RepeatedTimer(10, self.actOnPlayer)
 
