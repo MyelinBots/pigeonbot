8ded92cedbe8c7947b52cea1757642e3b05e1abe
James Trotter <james.trotter@tempmee.com>
2024-05-11 21:44:05 -0400

    fix: update pyircsdk module

--- bot.py
+++ bot.py
@@ -4,19 +4,24 @@ import os
 from modules.pigeon.main import PigeonModule
 
 host = os.getenv('HOST', 'irc.rizon.net')
-port = os.getenv('PORT', '6667')
+port = os.getenv('PORT', '6697')
 # convert port string to int
 port = int(port)
+ssl = os.getenv('SSL', 'True')
 nick = os.getenv('NICK', 'PigeonBot')
 channel = os.getenv('CHANNEL', '#toolbot')
+user = os.getenv('USER', 'PigeonBot')
 realname = os.getenv('REALNAME', 'PigeonBot')
 
 irc = IRCSDK(IRCSDKConfig(
-    host,
-    port,
-    nick,
-    channel,
-    realname
+    host=host,
+    port=port,
+    nick=nick,
+    # string false to boolean
+    ssl=ssl == 'True',
+    channel=channel,
+    user=user,
+    realname=realname
 ))
 
 pigeonModule = PigeonModule(irc)
